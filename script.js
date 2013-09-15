9a9d0d315dfd67ef3822648b5b52831ee83ff7d1
Rafa Latorre <sauco82@gmail.com>
2013-09-15 18:27:15 +0200

    fire working

--- script.js
+++ script.js
@@ -1,31 +1,54 @@
-var GREEN = "hsla(72, 57%, 40%, 0.9);";
-var RED = "hsla(6, 83%, 57%,1);";
+/** Request animframe **/
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 24);
+          };
+})();
 
-function getHsla(color) {
-  var h,s,l,a, colorString, hsla;
 
-  colorString = color.substring(5,color.length-1);
-  hsla = colorString.split(",");
-
-  h = parseInt(hsla[0]);
-  s = parseInt(hsla[1]);
-  l = parseInt(hsla[2]);
-  a = parseFloat(hsla[3]);
-
-  return {h:h, s:s, l:l, a:a}
+/** Color functions **/
+function Hsla(h,s,l,a) {
+  this.h = h;
+  this.s = s;
+  this.l = l;
+  this.a = a;
 }
 
-function hslaToColor(hsla){
-  var colorString = "hsla(" + hsla.h + "," 
-                  + hsla.s + "%,"
-                  + hsla.l + "%,"
-                  + hsla.a + ")";
+Hsla.prototype.color = function(){
+  var colorString = "hsla(" + this.h + "," 
+                  + this.s + "%,"
+                  + this.l + "%,"
+                  + this.a + ")";
   return colorString
 }
 
+Hsla.prototype.lighten = function(percent) {
+  if ((this.l + percent) < 100) {
+    this.l += percent;
+  } else {
+    this.l = 100;
+  }
+  return this.l
+}
 
+Hsla.prototype.darken = function(percent) {
+  if ((this.l - percent) > 0) {
+    this.l -= percent;
+  } else {
+    this.l = 0;
+  }
+  return this.l
+}
 
-function BaseLibrary(canvas_id){
+/** Main colors **/
+var GREEN = new Hsla(72, 57, 40, 0.9);
+var RED = new Hsla(6, 83, 57, 1);
+
+/** Canvas helper **/
+function CanvasHelper(canvas_id){
 
   var canvasElement = document.getElementById(canvas_id);
   
@@ -39,152 +62,120 @@ function BaseLibrary(canvas_id){
     this.centerX = this.width/2;
     this.centerY = this.height/2;
        
-    this.shapes = [];
   }
   
 }
 
-BaseLibrary.prototype.addSquare = function(size,color,x,y){
-  
-  var square = {
-    size: size,
-    color:color,
-    x: x,
-    y: y,
-    points: []
-  } 
+/** Fire **/
+function Fire(width, height, myCanvas,x,y){
+  var i,particle;
 
-  var initialX = x - square.size/2;
-  var initialY = y - square.size/2;
-
-  
-  square.points.push({x:initialX+=square.size , y:initialY});
-  square.points.push({x:initialX , y:initialY+=square.size});
-  square.points.push({x:initialX-=square.size , y:initialY});
-  square.points.push({x:initialX , y:initialY-=square.size}); 
-  
-  this.shapes.push(square);
-  
-}
-
-BaseLibrary.prototype.lighten = function(shape, percent) {
-  var hsla = getHsla(shape.color);
-
-  if (hsla.l + percent < 100) {
-    hsla.l += percent;
-  } else {
-    hsla.l = 100;
-  }
-
-  shape.color = hslaToColor(hsla);
-}
-
-
-BaseLibrary.prototype.darken = function(shape, percent) {
-  var hsla = getHsla(shape.color);
-
-  if (hsla.l + percent < 0) {
-    hsla.l -= percent;
-  } else {
-    hsla.l = 0;
-  }
-
-  shape.color = hslaToColor(hsla);
-}
-
-BaseLibrary.prototype.move = function(shape, a , b){
-  var i;
-  for (i=0;i<shape.points.length;i++){
-    shape.points[i].x += a;
-    shape.points[i].y += b;
-  }
-}
-
-BaseLibrary.prototype.rotate = function(shape, radians){
-  var i, cos = Math.cos(radians), sin = Math.sin(radians);
-
-  for (i=0;i<shape.points.length;i++){
-    shape.points[i].x = shape.points[i].x * cos - shape.points[i].x * sin;
-    shape.points[i].y = shape.points[i].y * cos + shape.points[i].y * sin;
-  }
-}
-
-BaseLibrary.prototype.drawShape = function(shape) {
-  var i;
-
-  this.context.beginPath();
-  this.context.moveTo(shape.initialX,shape.initialY);
-  
-  for (i=0; i<shape.points.length; i++) {
-    coordinate = shape.points[i];
-    this.context.lineTo(coordinate.x,coordinate.y);
-  }
-    
-  this.context.closePath();
-
-  this.context.fillStyle = shape.color;
-  
-  this.context.fill();
-}
-
-BaseLibrary.prototype.draw = function(){  
-  var i,coordinate;  
-  
-  this.context.clearRect(0, 0, this.width, this.height);
-
-  for (i=0; i<this.shapes.length; i++){
-    this.drawShape(this.shapes[i]);
-  }
-  
-}
-
-function Fire(canvas_id , width, height) {
-  this.base = new BaseLibrary("the_canvas");
+  this.x = x;
+  this.y = y;
   this.width = width;
   this.height = height;
+  this.myCanvas = myCanvas;
+  this.particles = [];
+  this.working = false;
+
+  this.left = x-width/2;
+  this.right = x+width/2;
+  this.bottom = y;
+  this.top = y-height;
+  
 }
 
 Fire.prototype.start = function(){
-  var i,base = this.base;
 
-  for (i=0; i<30; i++) {
-    base.addSquare(30, GREEN, base.centerX, base.height);
-  }  
-  
-  base.draw();
+  this.working = true;
 
+  this.particles = [];
+  for (i = 0; i < 70; i++) {
+    particle = new FireParticle(15, GREEN, this.x, this.y);
+    this.particles.push(particle);
+  };
   this.animate();
+}
+
+Fire.prototype.stop = function(){
+  this.working = false;
+}
+
+
+
+Fire.prototype.animate = function(){  
+  var i,coordinate,that = this;  
+
+  this.myCanvas.context.clearRect(0, 0, this.myCanvas.width, this.myCanvas.height);
+
+  for (i=0; i<that.particles.length; i++){
+    that.particles[i].draw(that.myCanvas.context);
+  }
+
+  that.move();
+  
+  if (this.working) {
+    requestAnimationFrame(function(){that.animate();});
+  }
 }
 
 Fire.prototype.move = function(){
   var i,xIncrement,yIncrement;
 
-  for (i=0; i<this.base.shapes.length; i++){
-    xIncrement = 10*Math.random() - 5;
-    yIncrement = -10*Math.random();
-    this.base.move(this.base.shapes[i], xIncrement, yIncrement);
-    this.base.lighten(this.base.shapes[i], 1);
+  for (i=0; i<this.particles.length; i++){
+
+    if (this.particles[i].inBoundaries(this.left, this.right, this.bottom, this.top)){
+      xIncrement = 10*Math.random() - 5;
+      yIncrement = -10*Math.random();
+      this.particles[i].x += xIncrement;
+      this.particles[i].y += yIncrement;
+      this.particles[i].angle += 0.01;    
+      this.particles[i].hsla.lighten(0.7);
+    } else {
+      this.particles[i] = new FireParticle(15, GREEN, this.x, this.y);
+    }
+
   }
 }
 
-Fire.prototype.animate = function(){
-  var that = this;
 
-  (function animloop(){
-    requestAnimFrame(animloop);
-    that.move();
-    that.base.draw();
-  })();
+/** Fire Particle **/
+function FireParticle(size,hsla,x,y){
+  this.size = size;
+  this.hsla = new Hsla(hsla.h, hsla.s, hsla.l, hsla.a);
+  this.x = x;
+  this.y = y;
+  this.angle = 0;
+  this.points = [];
+  
+  //yeah, there are easier ways to get a square
+  this.points.push({x: 0 , y: 0});
+  this.points.push({x: size , y: 0});
+  this.points.push({x: size , y: size});
+  this.points.push({x: 0 , y: size});
 }
 
-window.requestAnimFrame = (function(){
-  return  window.requestAnimationFrame       ||
-          window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame    ||
-          function( callback ){
-            window.setTimeout(callback, 1000 / 24);
-          };
-})();
+FireParticle.prototype.draw = function(context) {
+  var i;
 
+  context.save();
+  context.translate(this.x,this.y);
+  context.rotate(this.angle)
+  context.beginPath();
 
+  for (i=0; i<this.points.length; i++) {
+    coordinate = this.points[i];
+    context.lineTo(coordinate.x,coordinate.y);
+  }
+    
+  context.closePath();
 
+  context.fillStyle = this.hsla.color();
+  
+  context.fill();
+  context.restore();
+}
+
+FireParticle.prototype.inBoundaries = function(left, right, bottom, top){
+  return this.x >= left && this.x <= right && this.y <= bottom && this.y >= top
+}
